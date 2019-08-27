08ee970f76d9aceb381ef4a135bdce45fe64cd94
Vladimir Jotov <v.jotov@gmail.com>
2019-08-28 00:21:15 +0700

    initial app structure

---
+++ src/main/java/com/jotov/templier/templier/TemplierApplication.java
@@ -0,0 +1,13 @@
+package com.jotov.templier.templier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jotov/templier/templier/TemplierApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jotov.templier.templier;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TemplierApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
